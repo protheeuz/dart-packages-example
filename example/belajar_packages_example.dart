b8c3c2c92aa79b00252d35ea423e05fb2c522639
mathimatech <matimatech@proton.me>
2023-09-27 23:05:12 +0700

    first commit to my own packages

---
+++ example/belajar_packages_example.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_packages/hello.dart';
+
+void main() {
+  print(sayHello('Eko'));
+
+  var customer = Customer('Naswan');
+  var product = Product('iPhone');
+  var category = Category('Mobile');
+
+  print(customer);
+  print(product);
+  print(category);
+}
---
+++ lib/src/customer.dart
@@ -0,0 +1,17 @@
+class Customer {
+  String name;
+
+  Customer(this.name);
+}
+
+class Category {
+  String category;
+
+  Category(this.category);
+}
+
+class Product {
+  String product;
+
+  Product(this.product);
+}
